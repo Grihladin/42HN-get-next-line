afcda7117de87aff1ca8f8875ee831f2216fde4c
mratke <mratke@student.42heilbronn.de>
2024-10-28 21:07:21 +0100

    minor commit for improoved readability

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 21:34:23 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/28 21:01:09 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/28 21:07:17 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 static char	*read_line(int fd, char *current_line)
 {
 	char	*tmp;
-	char	*readed_chars;
-	int		readed_bytes;
+	char	*chars_readed;
+	int		bytes_readed;
 
-	readed_chars = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (readed_chars == NULL)
+	chars_readed = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (chars_readed == NULL)
 		return (free(current_line), NULL);
-	readed_chars[0] = '\0';
-	while (is_in_str(readed_chars, '\n') == 0)
+	chars_readed[0] = '\0';
+	while (is_in_str(chars_readed, '\n') == 0)
 	{
-		readed_bytes = read(fd, readed_chars, BUFFER_SIZE);
-		if (readed_bytes == -1)
-			return (free(readed_chars), free(current_line), NULL);
-		if (readed_bytes == 0)
+		bytes_readed = read(fd, chars_readed, BUFFER_SIZE);
+		if (bytes_readed == -1)
+			return (free(chars_readed), free(current_line), NULL);
+		if (bytes_readed == 0)
 			break ;
-		readed_chars[readed_bytes] = '\0';
-		tmp = ft_str_merge(current_line, readed_chars);
+		chars_readed[bytes_readed] = '\0';
+		tmp = ft_str_merge(current_line, chars_readed);
 		if (tmp == NULL)
-			return (free(readed_chars), free(current_line), NULL);
+			return (free(chars_readed), free(current_line), NULL);
 		free(current_line);
 		current_line = tmp;
 	}
-	free(readed_chars);
+	free(chars_readed);
 	return (current_line);
 }
 
